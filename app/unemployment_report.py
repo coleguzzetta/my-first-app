139d87032782fc72217e7eacda608fc9e84d1ec5
coleguzzetta <cag191@georgetown.edu>
2023-11-06 17:39:35 -0500

    Refactor

--- app/unemployment_report.py
+++ app/unemployment_report.py
@@ -49,7 +49,11 @@ data = parsed_response["data"]
 print("-------------------------")
 print("LATEST UNEMPLOYMENT RATE:")
 #print(data[0])
-print(f"{data[0]['value']}%", "as of", data[0]["date"])
+
+latest_rate = data[0]['value']
+latest_date = data[0]["date"]
+
+print(f"{latest_rate}%", "as of", latest_date)
 
 
 # Challenge B
@@ -79,9 +83,6 @@ fig = line(x=dates, y=rates, title="United States Unemployment Rate over time", 
 fig.show()
 
 user_address = input("In order to receive a copy of the report, please enter your email address: ")
-
-latest_rate = data[0]['value']
-latest_date = data[0]["date"]
 
 content = f"""
 <h1> Unemployment Report Email </h1>
